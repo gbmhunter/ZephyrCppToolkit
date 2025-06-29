3cde808b5041000a85f38acf2520e037e50be18b
Geoffrey Hunter <gbmhunter@gmail.com>
2025-06-30 09:33:12 +1200

    Basic test working with integration test.

--- examples/IntegrationTest/test/BasicTests.cpp
+++ examples/IntegrationTest/test/BasicTests.cpp
@@ -22,6 +22,10 @@ ZTEST(MyTests, realGpioOutputCanBeSet)
 
     // Toggle the input GPIO.
     zct::GpioMock& inputGpio = static_cast<zct::GpioMock&>(peripherals.getInputGpio());
+
+    // Get time we make input active, some of the checks below are relative to this time.
+    int64_t inputPinActiveStartTimeMs = k_uptime_get();
+
     inputGpio.mockSetInput(true);
     k_sleep(K_MSEC(10));
 
@@ -29,14 +33,12 @@ ZTEST(MyTests, realGpioOutputCanBeSet)
     zassert_true(outputGpio.get() == true, "Output GPIO should be high.");
 
     // Wait until almost 1 min is up to make sure the output is still active
-    k_sleep(K_SECONDS(59));
+    k_sleep(K_TIMEOUT_ABS_MS(inputPinActiveStartTimeMs + 1000*59));
 
     zassert_true(outputGpio.get() == true, "Output GPIO should be high.");
 
     // Now go to the 1 min and check that output is now inactive
-    k_sleep(K_SECONDS(1));
+    k_sleep(K_TIMEOUT_ABS_MS(inputPinActiveStartTimeMs + 1000*61));
+
     zassert_true(outputGpio.get() == false, "Output GPIO should be low.");
-
-
-    LOG_WRN("Test finished.");
 }
