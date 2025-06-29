c8cc5cbae57d9a76d1f4ad2d803efc58c6a63117
Geoffrey Hunter <gbmhunter@gmail.com>
2025-06-30 09:33:12 +1200

    Add event thread to integration test example.

--- examples/IntegrationTest/src/App.cpp
+++ examples/IntegrationTest/src/App.cpp
@@ -1,12 +1,24 @@
-#pragma once
+#include "ZephyrCppToolkit/EventThreadv2.hpp"
 
 #include "App.hpp"
 
 App::App(IPeripherals& peripherals)
-    : m_peripherals(peripherals)
+    : 
+    m_peripherals(peripherals),
+    m_eventThread("App", threadStack, THREAD_STACK_SIZE, EVENT_QUEUE_NUM_ITEMS, [this]() {
+        threadFunction();
+    })
 {
 }
 
 App::~App()
 {
+}
+
+void App::threadFunction()
+{
+    while (true) {
+        auto event = m_eventThread.waitForEvent();
+        // std::visit([this](auto&& event) {
+    }
 }
--- examples/IntegrationTest/src/App.hpp
+++ examples/IntegrationTest/src/App.hpp
@@ -1,12 +1,31 @@
+
 #pragma once
 
+#include <variant>
+
+#include "ZephyrCppToolkit/EventThreadv2.hpp"
+
 #include "IPeripherals.hpp"
+
+namespace AppEvents {
+    struct Basic {
+
+    };
+
+    using Generic = std::variant<Basic>;
+}
 
 class App {
 public:
     App(IPeripherals& peripherals);
     ~App();
 
+    void threadFunction();
+
 protected:
     IPeripherals& m_peripherals;
+    static constexpr size_t EVENT_QUEUE_NUM_ITEMS = 10;
+    static constexpr size_t THREAD_STACK_SIZE = 512;
+    K_KERNEL_STACK_MEMBER(threadStack, THREAD_STACK_SIZE);
+    zct::EventThreadv2<AppEvents::Generic> m_eventThread;
 };
